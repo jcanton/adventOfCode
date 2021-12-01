835be5927bb35042ffb41500025cbc1ef0dc990f
Jacopo <jacopo.canton@gmail.com>
2021-12-01 09:24:51 +0100

    day1_a working

--- src/main/kotlin/Day1/Day1.kt
+++ src/main/kotlin/Day1/Day1.kt
@@ -1,9 +1,27 @@
 package Day1
 
+import java.io.File
+import java.io.InputStream
+
 class Day1 {
 
     fun run() {
-        println("ciao")
+
+        val inputStream: InputStream = File("inputFiles/day1_input").inputStream()
+
+        var counter: Int = -1
+        var pDepth: Int = 0
+        var depth: Int
+
+        inputStream.bufferedReader().forEachLine {
+            depth = it.toInt()
+            if (depth > pDepth) {
+                counter += 1
+            }
+
+            pDepth = depth
+        }
+        println("nr. larger: ${counter}")
     }
 
 }
