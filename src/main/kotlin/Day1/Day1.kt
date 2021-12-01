298608ac627361931921985cbfb6e41e5dfd296f
Jacopo <jacopo.canton@gmail.com>
2021-12-01 09:56:25 +0100

    adding day1_b

--- src/main/kotlin/Day1/Day1.kt
+++ src/main/kotlin/Day1/Day1.kt
@@ -3,26 +3,29 @@ package Day1
 import java.io.File
 import java.io.InputStream
 
-class Day1 {
+class Day1(private val sP: Char) {
 
     fun run() {
 
         val pathname = "src/main/resources/inputFiles/day1_input"
         val inputStream: InputStream = File(pathname).inputStream()
 
-        var counter = -1
-        var pDepth = 0
-        var depth: Int
+        if (sP == 'a') {
+            var counter = -1
+            var pDepth = 0
+            var depth: Int
 
-        inputStream.bufferedReader().forEachLine {
-            depth = it.toInt()
-            if (depth > pDepth) {
-                counter++
+            inputStream.bufferedReader().forEachLine {
+                depth = it.toInt()
+                if (depth > pDepth) {
+                    counter++
+                }
+
+                pDepth = depth
             }
-
-            pDepth = depth
+            println("nr. larger: $counter")
+        } else {
+            println("nr. larger: 2")
         }
-        println("nr. larger: $counter")
     }
-
 }
