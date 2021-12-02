fbc91424443275a69f25933267fced225f696241
Tobias Rogg <tobias.rogg@juliusbaer.com>
2021-12-02 09:25:20 +0100

    formatting

--- src/main/kotlin/Day1/Day1a.kt
+++ src/main/kotlin/Day1/Day1a.kt
@@ -1,27 +1,16 @@
 package Day1
 
-import java.io.File
-import java.io.InputStream
 
+class Day1a(pathname: String) : IDay1(pathname) {
 
-class Day1a(pathname:String ) : IDay1(pathname) {
+    override fun run() {
+        var pDepth = 0
+        inputStream.bufferedReader().forEachLine {
+            depth = it.toInt()
+            if (depth > pDepth) counter++
 
-     override fun run() {
-
-
-
-
-            var pDepth = 0
-            inputStream.bufferedReader().forEachLine {
-                depth = it.toInt()
-                if (depth > pDepth) counter++
-
-                pDepth = depth
-            }
-
-
-
+            pDepth = depth
+        }
         println("nr. larger: $counter")
-
     }
 }
