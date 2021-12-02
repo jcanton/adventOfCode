dc7c37c95f948502b62bfedb0f05b5a6f01d5414
Tobias Rogg <tobias.rogg@juliusbaer.com>
2021-12-02 09:29:46 +0100

    rewrite main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,12 @@
 import Day1.Day1a
 import Day1.Day1b
+import Day1.IDay1
 
 fun main() {
     val pathname = "src/main/resources/inputFiles/day1_input"
-    Day1a(pathname).run()
-    Day1b(pathname).run()
+    var day1: IDay1
+    day1 = Day1a(pathname)
+    day1.run()
+    day1 = Day1b(pathname)
+    day1.run()
 }
