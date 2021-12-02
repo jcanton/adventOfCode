9b9a31d6c2b6a0f3bd254eedd75de1625dbc6d33
Tobias Rogg <tobias.rogg@juliusbaer.com>
2021-12-02 09:28:51 +0100

    cool

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,8 @@
 import Day1.Day1a
 import Day1.Day1b
-import Day1.IDay1
 
 fun main() {
     val pathname = "src/main/resources/inputFiles/day1_input"
-    val day1: IDay1
     Day1a(pathname).run()
     Day1b(pathname).run()
 }
