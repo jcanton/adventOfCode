91d9baa47a719eb41bf50f51518079bad3d0bc4d
Jacopo <jacopo.canton@gmail.com>
2021-12-03 11:27:11 +0100

    Implemented Day2b

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,12 @@
 import Day2.Day2
+import Day2.Day2a
+import Day2.Day2b
 
 fun main() {
     val pathname = "src/main/resources/inputFiles/day2_input"
-    val day: Day2
-    day = Day2(pathname)
+    var day: Day2
+    day = Day2a(pathname)
     day.run()
-    //day = Day1b(pathname)
-    //day.run()
+    day = Day2b(pathname)
+    day.run()
 }
