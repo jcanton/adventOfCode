2ac67763b00923f895749fa19079d0f8454bcdbc
Tobias Rogg <tobias.rogg@juliusbaer.com>
2021-12-03 10:46:54 +0100

    test

--- src/main/kotlin/Day1/Day1Test.kt
+++ src/main/kotlin/Day1/Day1Test.kt
@@ -1,12 +1,15 @@
 package Day1
 
 import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertEquals
 
 
 internal class Day1Test {
+    val pathname = "src/main/resources/inputFiles/day1_input"
 
     @Test
     fun givenFileProducesRightCount() {
-        
+        assertEquals(1559, Day1a(pathname).countInc())
+        assertEquals(1600, Day1b(pathname).countInc())
     }
 }
